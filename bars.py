c37363729db133eb7a375ed4912eb4fa53f89ebc
Dmitry Pryanichnikov <420600@gmail.com>
2017-10-26 15:47:28 +0500

    Update bars.py
    Add comments to get_distance function

--- bars.py
+++ bars.py
@@ -30,23 +30,26 @@ def get_closest_bar(bars, longitude, latitude):
 
 
 def get_distance(cur_longitude, cur_latitude, place_longitude, place_latitude):
-    radius = 6372795
-    lat1, lat2, long1, long2 = map(math.radians,
+    # All calculations are taken from here: http://gis-lab.info/qa/great-circles.html
+    radius = 6372795 # Earth sphere radius in meters
+    lat1, lat2, long1, long2 = map(math.radians, # Convert coords to radians
                                    [
                                        float(cur_latitude),
                                        place_latitude,
                                        float(cur_longitude),
                                        place_longitude
                                    ])
-
+    # cos and sin of latitude
     cl1 = math.cos(lat1)
     cl2 = math.cos(lat2)
     sl1 = math.sin(lat1)
     sl2 = math.sin(lat2)
+    # longitude difference
     delta = long2 - long1
     cdelta = math.cos(delta)
     sdelta = math.sin(delta)
-
+    
+    # calculating the length of a large circle
     y = math.sqrt(math.pow(cl2 * sdelta, 2) + math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2))
     x = sl1 * sl2 + cl1 * cl2 * cdelta
     ad = math.atan2(y, x)
